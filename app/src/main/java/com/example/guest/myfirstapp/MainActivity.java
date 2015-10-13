5f22ff8edae9cdce4704ccc35850d76e026bc165
Yvonne <student@epicodus.com>
2015-10-13 09:27:14 -0700

    Initial commit

---
+++ app/src/main/java/com/example/guest/myfirstapp/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.guest.myfirstapp;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    private FactBook mFactBook = new FactBook();
+    private ColorWheel mColorWheel = new ColorWheel();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Declare the View variables and assign the Views from the layout file
+        final TextView factLabel = (TextView) findViewById(R.id.factTextView);
+        final Button showFactButton = (Button) findViewById(R.id.showFactButton);
+        final RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String fact = mFactBook.getFact();
+                factLabel.setText(fact);
+
+                int color = mColorWheel.getColor();
+                relativeLayout.setBackgroundColor(color);
+                showFactButton.setTextColor(color);
+
+            }
+        };
+
+
+        showFactButton.setOnClickListener(listener);
+
+        // Toast.makeText(this, "Activity was created!", Toast.LENGTH_LONG).show();
+        Log.d(TAG, "Logging from the onCreate() method!");
+
+    }
+
+}
